b8a4c3c73332c53b4775abdb20c062ee54b91cc2
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-08 12:05:56 +0100

    Delete buttons on gym_classes/index working.

--- app_code/controllers/gym_classes_controller.rb
+++ app_code/controllers/gym_classes_controller.rb
@@ -4,5 +4,15 @@ require_relative('../models/gym_class')
 also_reload('../models/*')
 
 get '/gym_classes' do
+  @gym_classes = GymClass.all
   erb (:"gym_classes/index")
+end
+
+get '/gym_classes/new' do
+  erb (:"gym_classes/new")
+end
+
+post '/gym_classes/:id/delete' do
+  GymClass.destroy(params[:id])
+  redirect to ('/gym_classes')
 end
