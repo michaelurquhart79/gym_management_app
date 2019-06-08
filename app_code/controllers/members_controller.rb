7a3e9f2732319f1d2ae8fff663a132180d50b44c
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-08 15:40:29 +0100

    Delete member functionality working in app.

--- app_code/controllers/members_controller.rb
+++ app_code/controllers/members_controller.rb
@@ -17,3 +17,9 @@ post '/members' do
   new_member.save()
   redirect to ('/members')
 end
+
+post '/members/:id/delete' do
+  member = Member.find_by_id(params[:id])
+  member.delete()
+  redirect to ('/members')
+end
