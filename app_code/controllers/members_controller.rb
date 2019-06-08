444bc5caba0377fef3b33b68326206b2bdfbac95
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-08 15:30:25 +0100

    Create new member functionality working in app.

--- app_code/controllers/members_controller.rb
+++ app_code/controllers/members_controller.rb
@@ -7,3 +7,13 @@ get '/members' do
   @members = Member.all
   erb (:"members/index")
 end
+
+get '/members/new' do
+  erb (:"members/new")
+end
+
+post '/members' do
+  new_member = Member.new(params)
+  new_member.save()
+  redirect to ('/members')
+end
