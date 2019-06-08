ddc4b16dda38bb5e06ade140df94be8b46570d25
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-08 12:34:36 +0100

    'new.erb' gym_classes view (form) and functionality working.

--- app_code/controllers/gym_classes_controller.rb
+++ app_code/controllers/gym_classes_controller.rb
@@ -12,6 +12,12 @@ get '/gym_classes/new' do
   erb (:"gym_classes/new")
 end
 
+post '/gym_classes' do
+  gym_class = GymClass.new(params)
+  gym_class.save
+  redirect to ('/gym_classes')
+end
+
 post '/gym_classes/:id/delete' do
   GymClass.destroy(params[:id])
   redirect to ('/gym_classes')
