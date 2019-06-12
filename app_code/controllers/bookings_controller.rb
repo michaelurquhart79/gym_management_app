01777e59dd45a9ff4c38e91ae8a659dbbbdc3a4d
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-12 09:14:58 +0100

    fixed typo line 7: models not modles.

--- app_code/controllers/bookings_controller.rb
+++ app_code/controllers/bookings_controller.rb
@@ -4,7 +4,7 @@ require('pry-byebug')
 require_relative('../models/gym_class')
 require_relative('../models/booking')
 require_relative('../models/member')
-also_reload('../modles/*')
+also_reload('../models/*')
 
 get '/bookings' do
   @bookings = Booking.all
